bb242783ce33d2c718f8a365c542d7262d0c36c5
Janaina Gomes <90685145+jangomes@users.noreply.github.com>
2023-01-03 18:23:34 +0000

    Add files via upload

---
+++ styles/global.js
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+
+
+export const globalStyles = StyleSheet.create({
+    container1:{
+        flex: 1,
+        marginTop: 50,
+      },
+
+    container:{
+      flex: 1,
+      alignItems: 'center',
+      marginTop: 50,
+    },
+
+TitleText:{ 
+        fontSize: 20,
+        fontWeight: 'bold',
+        justifyContent: 'center',
+      },
+
+HeaderText:{ 
+
+        textAlign: 'center',
+        color: '#fff',
+        fontSize: 20,
+        fontWeight: 'bold'
+      },
+
+Header:{ 
+
+        height: 80,
+        paddingTop: 28,
+        backgroundColor: 'black'
+      },
+
+      paragraph:{   
+        marginVertical: 8,
+        lineHeight: 20,
+        },
+  } )
+
