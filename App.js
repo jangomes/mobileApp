9caadeed9f3aca6454fbf1147556fb58ca5253d4
Janaina Gomes <90685145+jangomes@users.noreply.github.com>
2023-01-03 18:23:13 +0000

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Mainscreen from './home/Mainscreen';
+import Details from './home/Details';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+
+      <Text>Welcome to your Map App!</Text>
+
+            
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+const Tab = createBottomTabNavigator();
+
+
+export default function App() {
+ 
+
+  return (
+    <NavigationContainer>
+
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused 
+              ? 'ios-home' 
+              : 'ios-home-outline';
+            }
+            else if (route.name === 'Main Screen') {
+              iconName = focused 
+              ? 'ios-globe' 
+              : 'ios-globe-outline';
+            }
+            else if (route.name === 'Details') {
+              iconName = focused 
+              ? 'ios-pin' 
+              : 'ios-pin-outline';
+            }
+             // You can return any component that you like here!
+             return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+
+        >      
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Main Screen" component={Mainscreen} />
+          <Tab.Screen name="Details" component={Details} />
+         
+        </Tab.Navigator>
+      </NavigationContainer>
+    
+    );
+  }
+
+  
